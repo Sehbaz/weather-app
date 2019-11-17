7b614fdf9b9f7fc625630d6860893150e025754e
Sehbaz Rafik <sehbazrafik@Sehbazs-MacBook-Pro.local>
2019-11-17 19:27:59 +0000

    css update

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import "./Form.css";
 import Axios from "axios";
+
 class Form extends Component {
   // setting state to default value
-  state = { cityName: "", country: "", tech: [], temp: "", newsTitle: "" };
+  state = {
+    cityName: "",
+    country: "",
+    tech: [],
+    temp: "",
+    newsTitle: "",
+    cityapi: "",
+    countryapi: ""
+  };
 
   //handling the on click submit event
   handleSubmit = async event => {
@@ -12,13 +21,16 @@ class Form extends Component {
     const response = await Axios.get(
       `https://openweathermap.org/data/2.5/weather?q=${this.state.cityName},${this.state.country}&appid=b6907d289e10d714a6e88b30761fae22`
     );
-    this.setState({ temp: response.data.main.temp });
+    this.setState({
+      temp: response.data.main.temp,
+      countryapi: response.data.sys.country,
+      cityapi: response.data.name + ", "
+    });
     // this will fetch the latest news in the tech industry
     const news = await Axios.get(
-      `https://newsapi.org/v2/top-headlines?q=tech&apiKey=7035a2ce18004b138c9f980fe58a0f21&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?q=tech&apiKey=7035a2ce18004b138c9f980fe58a0f21&pageSize=15`
     );
     this.setState({ tech: news.data.articles, newsTitle: "tech today" });
-
     // resetting to default values
     this.setState({ cityName: "", country: "" });
   };
@@ -26,20 +38,26 @@ class Form extends Component {
   render() {
     // mapping the array of top tech news
     const news = this.state.tech.map(elements => {
-      return <p>{elements.title}</p>;
+      return <li>{elements.title}</li>;
     });
-
     return (
       <div className="main-container">
         <div className="news-container">
           <h1>{this.state.newsTitle}</h1>
-          <div>{news}</div>
+          <div>
+            <ul>{news}</ul>
+          </div>
         </div>
-        <div className="temp-container">
-          <h2>{this.state.temp}</h2>
-        </div>
+
         <div className="form-container">
           {" "}
+          <div className="temp-container">
+            <h2>{this.state.temp}</h2>
+            <p>
+              {this.state.cityapi}
+              {this.state.countryapi}
+            </p>
+          </div>
           <form onSubmit={this.handleSubmit}>
             <input
               type="text"
@@ -57,7 +75,7 @@ class Form extends Component {
               onChange={event => this.setState({ country: event.target.value })}
               required
             ></input>
-            <button>GO</button>
+            <button className="gobtn">GO</button>
           </form>
         </div>
       </div>
