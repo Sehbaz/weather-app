3a93b41fe11f457272c065bf9bea0955e85bc3a0
Sehbaz Rafik <sehbazrafik@Sehbazs-MacBook-Pro.local>
2019-11-17 01:31:05 +0000

    country add in form and top news fetched

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import "./App.css";
 import Form from "./Components/Form";
 
 class App extends Component {
-  // this function will fetch the data from From.js and display it in app.js
-  state = {
-    temp: ""
-  };
-  getWeather = weatherinfo => {
-    console.log(weatherinfo.main.temp);
-    this.setState({ temp: weatherinfo.main.temp });
-  };
   render() {
     return (
       <div>
         <h2>Hello world, checkout today's weather!</h2>
-        <Form onSubmit={this.getWeather}></Form>
-        <h1>{this.state.temp}</h1>
+        <Form></Form>
       </div>
     );
   }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,30 +3,53 @@ import "./Form.css";
 import Axios from "axios";
 class Form extends Component {
   // setting state to default value
-  state = { cityName: "" };
+  state = { cityName: "", country: "", tech: [], temp: "" };
 
   //handling the on click submit event
   handleSubmit = async event => {
     event.preventDefault();
-    // using axios for fetch the data form the api
+    // using axios for fetch the data form the api for weather report
     const response = await Axios.get(
-      `https://openweathermap.org/data/2.5/weather?q=${this.state.cityName}&appid=b6907d289e10d714a6e88b30761fae22`
+      `https://openweathermap.org/data/2.5/weather?q=${this.state.cityName},${this.state.country}&appid=b6907d289e10d714a6e88b30761fae22`
     );
-    this.props.onSubmit(response.data);
-    this.setState({ cityName: "" });
+    this.setState({ temp: response.data.main.temp });
+    // this will fetch the latest news in the tech industry
+    const news = await Axios.get(
+      `https://newsapi.org/v2/top-headlines?q=tech&apiKey=7035a2ce18004b138c9f980fe58a0f21`
+    );
+    this.setState({ tech: news.data.articles });
+    // resetting to default values
+    this.setState({ cityName: "", country: "" });
   };
 
   render() {
+    // mapping the array of top tech news
+    const news = this.state.tech.map(elements => {
+      return <p>{elements.title}</p>;
+    });
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="enter your city"
-          value={this.state.cityName}
-          onChange={event => this.setState({ cityName: event.target.value })}
-        ></input>
-        <button>GO</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="city: London"
+            value={this.state.cityName}
+            onChange={event => this.setState({ cityName: event.target.value })}
+            required
+          ></input>
+          <input
+            type="text"
+            placeholder="country: UK"
+            value={this.state.country}
+            onChange={event => this.setState({ country: event.target.value })}
+            required
+          ></input>
+          <button>GO</button>
+        </form>
+        <h2>{this.state.temp}</h2>
+        <div>{news}</div>
+      </div>
     );
   }
 }
